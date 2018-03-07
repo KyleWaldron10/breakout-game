f498369aa345b12334a59b3eb819e64f738cba27
KyleWaldron10 <35769349+KyleWaldron10@users.noreply.github.com>
2018-03-07 14:55:46 +0000

    Add files via upload

---
+++ draw bricks.js
@@ -0,0 +1,120 @@
+//Setup some variables for the canvas
+var canvas = document.getElementById("myCanvas");
+//the ctx variables sets up the 2nd context so we can paint on it
+var ctx = canvas.getContext("2d");
+//Setup other variables for the ball size and position
+var BallRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+//Now define the paddle
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+//setup some bricks
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30; 
+
+//Hold the bricks in a two-dimensional array - think of it as rows and columns
+var brick = [];
+for(c=0; c<brickColumnCount; c++) {
+	bricks [c] = [];
+	for(r=0; r<brickRowCount; r++) {
+		bricks[c][r] = { x: 0, y: o, status: 1 };
+	}
+}
+
+//this function draws the bricks 
+function drawBricks() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++
+
+	
+//This function draws the ball on the canvas
+function drawBall() {
+ctx.beginPath();
+ctx.arc(x, y, BallRadius, 0, Math.PI*2);
+ctx.fillStyle = "#0095DD";
+ctx.fill();
+ctx.closePath();
+}
+
+//This function draws the paddle
+function drawPaddle(){ctx.beginPath();
+ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+ctx.fillStyle ="#0095DD";
+ctx.fill();
+ctx.closePath();
+}
+
+function draw() {
+	//clear the canvas
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	//Draw the ball
+	drawBall();
+	//draw the paddle
+	drawPaddle();
+	
+	//Bounce the ball off side walls - if it drops off the bottom - Game Over!
+	if(x + dx > canvas.width-BallRadius || x + dx < BallRadius) {
+				dx = -dx;
+	}
+
+
+	// check if the ball is hitting the paddle
+	if(y + dy < BallRadius) {
+		  dy = -dy;
+	} else if(y + dy > canvas.height-BallRadius) {
+    if(x > paddleX && x < paddleX + paddleWidth) {
+			dy = -dy;
+	}
+	else { 
+		alert("GAME OVER");
+		document.location.reload();
+	}
+	}	
+	
+	if(rightPressed && paddleX < canvas.width-paddleWidth){
+		paddleX += 7;
+	}
+	else if(leftPressed && paddleX > 0){
+		paddleX -= 7;
+	}
+	
+	x += dx;
+	y += dy;
+}
+
+//monitor the documents for events that move the paddle
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup" , keyUpHandler, false);
+
+//Define the functions to handle the keyDown or keyUp events
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = true;
+	}
+	else if(e.keyCode ==37) {
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = false;
+	}
+}
+
+setInterval(draw, 10);
